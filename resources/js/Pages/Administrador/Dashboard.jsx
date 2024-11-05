50473cd67ca1e67f26c39c706e8160bb4d3aafd6
JMarceloQ <jquispe.231ds24@istta.edu.pe>
2024-11-05 16:46:16 -0500

    cambie de color

--- resources/js/Pages/Administrador/Dashboard.jsx
+++ resources/js/Pages/Administrador/Dashboard.jsx
@@ -24,19 +24,19 @@ export default function Dashboard() {
     return (
         <AuthenticatedLayout
             header={
-                <h2 className="text-3xl font-bold leading-tight text-white bg-gradient-to-r from-red-700 to-red-900 p-4 rounded-lg shadow-lg text-center">
+                <h2 className="text-3xl font-bold leading-tight text-white bg-gradient-to-r from-[#800020] to-[#6A4E3C] p-4 rounded-lg shadow-lg text-center">
                     Panel de Control del Administrador
                 </h2>
             }
         >
             <Head title="Panel de Control - Centro de Idiomas" />
 
-            {/* Fondo con gradiente suave */}
-            <div className="py-12 bg-gradient-to-b from-red-600 to-red-800 min-h-screen">
+            {/* Fondo con gradiente suave de granate a color piel */}
+            <div className="py-12 bg-gradient-to-b from-[#800020] to-[#F5D0A9] min-h-screen">
                 <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
 
                     {/* Card Principal - Bienvenida */}
-                    <div className="overflow-hidden bg-white shadow-lg sm:rounded-lg mb-10">
+                    <div className="overflow-hidden bg-amber-50 shadow-xl sm:rounded-lg mb-10">
                         <div className="p-8 text-gray-800 text-center">
                             {/* Sección de imágenes */}
                             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 mb-6">
@@ -68,20 +68,20 @@ export default function Dashboard() {
                                 </div>
                             </div>
 
-                            <h3 className="text-2xl font-semibold mb-4 text-red-700">
+                            <h3 className="text-2xl font-semibold mb-4 text-[#6A4E3C]">
                                 ¡Bienvenido al Panel de Control!
                             </h3>
-                            <p className="mb-6 text-gray-600">
+                            <p className="mb-6 text-[#4B4B4B]">
                                 Aquí podrás gestionar todas las configuraciones necesarias para el correcto funcionamiento de la aplicación.
                                 Utiliza las opciones en el menú para navegar entre las distintas secciones y realizar las acciones que necesites.
                             </p>
-                            <h3 className="text-2xl font-semibold mb-4 text-red-800">
+                            <h3 className="text-2xl font-semibold mb-4 text-[#800020]">
                                 Nuestros Valores
                             </h3>
 
                             {/* Información adicional al hacer clic */}
                             {info && (
-                                <div className="bg-red-300 p-4 rounded-lg shadow-md mb-6 text-center text-gray-800">
+                                <div className="bg-[#F5D0A9] p-4 rounded-lg shadow-md mb-6 text-center text-[#800020]">
                                     <p>{info}</p>
                                 </div>
                             )}
@@ -90,38 +90,38 @@ export default function Dashboard() {
                             <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 mb-6">
                                 {/* Card Responsabilidad */}
                                 <div
-                                    className="bg-gradient-to-r from-red-700 to-red-900 p-4 rounded-lg shadow-lg hover:shadow-2xl transition-transform transform hover:scale-105 cursor-pointer"
+                                    className="bg-[#800020] p-4 rounded-lg shadow-lg hover:shadow-2xl hover:bg-[#6A4E3C] transition-all cursor-pointer"
                                     onClick={() => handleCardClick('Responsabilidad')}
                                     aria-label="Responsabilidad"
                                     tabIndex="0"
                                 >
-                                    <FaClipboardCheck className="text-yellow-400 text-4xl mb-4 transition-transform transform hover:scale-110 hover:text-yellow-500" />
-                                    <h4 className="text-lg font-semibold text-white">Responsabilidad</h4>
-                                    <p className="text-gray-100 text-sm">Nos comprometemos a cumplir con nuestras tareas y obligaciones.</p>
+                                    <FaClipboardCheck className="text-[#F5D0A9] text-4xl mb-4 transition-transform transform hover:scale-110 hover:text-[#F2C49B]" />
+                                    <h4 className="text-lg font-semibold text-[#F5D0A9]">Responsabilidad</h4>
+                                    <p className="text-[#F5D0A9] text-sm">Nos comprometemos a cumplir con nuestras tareas y obligaciones.</p>
                                 </div>
 
                                 {/* Card Empatía */}
                                 <div
-                                    className="bg-gradient-to-r from-red-700 to-orange-600 p-4 rounded-lg shadow-lg hover:shadow-2xl transition-transform transform hover:scale-105 cursor-pointer"
+                                    className="bg-[#800020] p-4 rounded-lg shadow-lg hover:shadow-2xl hover:bg-[#6A4E3C] transition-all cursor-pointer"
                                     onClick={() => handleCardClick('Empatía')}
                                     aria-label="Empatía"
                                     tabIndex="0"
                                 >
-                                    <FaHandsHelping className="text-yellow-400 text-4xl mb-4 transition-transform transform hover:scale-110 hover:text-yellow-500" />
-                                    <h4 className="text-lg font-semibold text-white">Empatía</h4>
-                                    <p className="text-gray-100 text-sm">Escuchamos y comprendemos las necesidades de los demás.</p>
+                                    <FaHandsHelping className="text-[#F5D0A9] text-4xl mb-4 transition-transform transform hover:scale-110 hover:text-[#F2C49B]" />
+                                    <h4 className="text-lg font-semibold text-[#F5D0A9]">Empatía</h4>
+                                    <p className="text-[#F5D0A9] text-sm">Escuchamos y comprendemos las necesidades de los demás.</p>
                                 </div>
 
                                 {/* Card Creatividad */}
                                 <div
-                                    className="bg-gradient-to-r from-red-800 to-orange-500 p-4 rounded-lg shadow-lg hover:shadow-2xl transition-transform transform hover:scale-105 cursor-pointer"
+                                    className="bg-[#800020] p-4 rounded-lg shadow-lg hover:shadow-2xl hover:bg-[#6A4E3C] transition-all cursor-pointer"
                                     onClick={() => handleCardClick('Creatividad')}
                                     aria-label="Creatividad"
                                     tabIndex="0"
                                 >
-                                    <FaLightbulb className="text-yellow-400 text-4xl mb-4 transition-transform transform hover:scale-110 hover:text-yellow-500" />
-                                    <h4 className="text-lg font-semibold text-white">Creatividad</h4>
-                                    <p className="text-gray-100 text-sm">Buscamos soluciones innovadoras para superar desafíos.</p>
+                                    <FaLightbulb className="text-[#F5D0A9] text-4xl mb-4 transition-transform transform hover:scale-110 hover:text-[#F2C49B]" />
+                                    <h4 className="text-lg font-semibold text-[#F5D0A9]">Creatividad</h4>
+                                    <p className="text-[#F5D0A9] text-sm">Buscamos soluciones innovadoras para superar desafíos.</p>
                                 </div>
                             </div>
 
@@ -129,26 +129,26 @@ export default function Dashboard() {
                             <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-2 gap-6 mb-6">
                                 {/* Card Comunicación */}
                                 <div
-                                    className="bg-gradient-to-r from-red-900 to-red-800 p-4 rounded-lg shadow-lg hover:shadow-2xl transition-transform transform hover:scale-105 cursor-pointer"
+                                    className="bg-[#800020] p-4 rounded-lg shadow-lg hover:shadow-2xl hover:bg-[#6A4E3C] transition-all cursor-pointer"
                                     onClick={() => handleCardClick('Comunicación')}
                                     aria-label="Comunicación"
                                     tabIndex="0"
                                 >
-                                    <FaComments className="text-yellow-400 text-4xl mb-4 transition-transform transform hover:scale-110 hover:text-yellow-500" />
-                                    <h4 className="text-lg font-semibold text-white">Comunicación</h4>
-                                    <p className="text-gray-100 text-sm">Mantenemos un diálogo abierto y honesto.</p>
+                                    <FaComments className="text-[#F5D0A9] text-4xl mb-4 transition-transform transform hover:scale-110 hover:text-[#F2C49B]" />
+                                    <h4 className="text-lg font-semibold text-[#F5D0A9]">Comunicación</h4>
+                                    <p className="text-[#F5D0A9] text-sm">Mantenemos un diálogo abierto y honesto.</p>
                                 </div>
 
                                 {/* Card Integridad */}
                                 <div
-                                    className="bg-gradient-to-r from-red-900 to-red-800 p-4 rounded-lg shadow-lg hover:shadow-2xl transition-transform transform hover:scale-105 cursor-pointer"
+                                    className="bg-[#800020] p-4 rounded-lg shadow-lg hover:shadow-2xl hover:bg-[#6A4E3C] transition-all cursor-pointer"
                                     onClick={() => handleCardClick('Integridad')}
                                     aria-label="Integridad"
                                     tabIndex="0"
                                 >
-                                    <FaShieldAlt className="text-yellow-400 text-4xl mb-4 transition-transform transform hover:scale-110 hover:text-yellow-500" />
-                                    <h4 className="text-lg font-semibold text-white">Integridad</h4>
-                                    <p className="text-gray-100 text-sm">Actuamos con honestidad y ética en todas nuestras interacciones.</p>
+                                    <FaShieldAlt className="text-[#F5D0A9] text-4xl mb-4 transition-transform transform hover:scale-110 hover:text-[#F2C49B]" />
+                                    <h4 className="text-lg font-semibold text-[#F5D0A9]">Integridad</h4>
+                                    <p className="text-[#F5D0A9] text-sm">Actuamos con honestidad y ética en todas nuestras interacciones.</p>
                                 </div>
                             </div>
                         </div>
